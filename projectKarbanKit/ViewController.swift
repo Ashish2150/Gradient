f2bd1635c0711e4d6fb01103e9fd250e1a2d79d6
Ashish2150 <ashishmauryait@gmail.com>
2018-01-17 00:04:24 +0530

    Revert "Change Text"
    This reverts commit a1cde8c8d01b746b11ef51db47134a365e807ed4.

--- projectKarbanKit/ViewController.swift
+++ projectKarbanKit/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         lbltext.text = "NewFile \n me"
         lbltext.backgroundColor = UIColor.green
         // Do any additional setup after loading the view, typically from a nib.
